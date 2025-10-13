3a8f9848f21f46e2cb6acf92baf7e57d4e285601
Niyubwayo Irakoze Elie <n.elie@alustudent.com>
2025-10-13 23:34:35 +0200

    Improved create_environment.sh to handle error gracefully and in clean way

--- create_environment.sh
+++ create_environment.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+# Ask user for their name
 echo -n "Please enter your name: "
 read -r name
 
-mkdir -p submission_reminder_"$name"
+# Check if the name is not empty string
+if [ -z "$name" ]; then
+    echo "Error: Name cannot be empty!"
+    exit 1
+fi
 
-cd submission_reminder_"$name"
+# Creating main_dir which is submission_reminder_{name}
+main_dir="submission_reminder_${name}"
 
-mkdir -p app modules assets config
+if [ -d "$main_dir" ]; then
+    # Ask if they want to overwrite if the script is being run many times 
+    echo "Warning: Directory already exists. Do you want to overwrite it? (y/n)"
+    read -p "Choice: " choice
 
-touch startup.sh
+    if [ "$choice" != "y" ]; then
+        echo "Setup cancelled."
+        exit 0
+    fi
+    rm -rf "$main_dir"
+fi
 
-chmod +x startup.sh
+mkdir -p "$main_dir"
+
+# Creating subdirectories
+mkdir -p "$main_dir/app"
+mkdir -p "$main_dir/modules"
+mkdir -p "$main_dir/assets"
+mkdir -p "$main_dir/config"
